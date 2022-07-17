25143fdbb07c94a337e63915c8cef77c3825f63e
Naomi Justin <63893978+naomijustin@users.noreply.github.com>
2022-07-17 17:40:38 +1000

    Updated for 2 arm movements

---
+++ js/esprino_tupperware-with-arms-3Movements.js
@@ -0,0 +1,134 @@
+// ====== TUPPERWARE BOT WITH ARMS 3 MOVEMENTS FOR ESPRINO ========
+
+var smarti = require("Smartibot");
+var counting = false;
+var count = 0;
+var step = 2;
+
+function go(drive){
+if (drive == "B"){
+     // Back/Down
+    smarti.setLEDs([50,0,50],[50,0,50]);
+    smarti.setMotor(1,-0.7); //Back
+    smarti.setMotor(2,0.7); //Back    
+    step = 4;
+    counting = true;
+ }
+ if (drive == "R"){
+     // Right
+    smarti.setLEDs([50,0,0],[50,0,0]);
+    smarti.setMotor(1,-0.6); // Right
+    smarti.setMotor(2,-0.6); // Right
+    step = 2;
+    counting = true;
+ }
+
+  if (drive == "SR"){
+    smarti.setLEDs([50,0,50],[60,0,50]);
+    smarti.setMotor(1,-0.55);
+    smarti.setMotor(2,0.55);
+    step = 1;
+    counting = true;
+ }
+ if (drive == "L"){
+    smarti.setLEDs([0,0,50],[0,0,50]);
+    smarti.setMotor(1,0.6); //Left
+    smarti.setMotor(2,0.6); //Left
+    step = 2;
+    counting = true;
+    
+ }
+ if (drive == "SL"){
+   smarti.setLEDs([50,0,50],[50,0,50]);
+   smarti.setMotor(1,0.55);
+   smarti.setMotor(2,-0.55);
+   step = 1;
+   counting = true;
+ }
+ else if (drive == "F"){
+    // Up
+    smarti.setLEDs([50,0,50],[60,0,50]);
+    smarti.setMotor(1,0.7); //Forward
+    smarti.setMotor(2,-0.7); //Forward    
+    step = 4;
+    counting = true;
+ }
+
+ // === SERVO ARMS ===
+ else if (drive == "L0") {
+   // Servo Left 0% - 50
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(1,50);
+}
+ else if (drive == "L50") {
+   // Servo Left 50% - 65
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(1,65);
+}
+else if (drive == "L100") {
+   // Servo Left 100% - 80
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(1,80);
+}
+else if (drive == "R0") {
+   // Servo Right 0% - 50
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(2,50);
+}
+else if (drive == "R50") {
+   // Servo Right 50% - 35
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(2,35);
+}
+else if (drive == "R100") {
+   // Servo Right 100% - 20
+   smarti.setLEDs([0,50,0],[0,50,0]);
+   smarti.setServo(2,20);
+}
+
+// === TILT PHONE ===
+else if (drive == "TU") {
+   smarti.setLEDs([50,0,50],[50,0,50]);
+   smarti.setMotor(3,0.6);
+   smarti.setMotor(4,0.6);
+   setTimeout (function() {
+      smarti.setMotor(3, 0);
+      smarti.setMotor(4, 0);
+   }, 50);
+}
+
+else if (drive == "TD") {
+   smarti.setLEDs([50,0,50],[50,0,50]);
+   smarti.setMotor(3,-0.6); 
+   smarti.setMotor(4,-0.6);
+   setTimeout (function() {
+      smarti.setMotor(3, 0);
+      smarti.setMotor(4, 0);
+   }, 50);
+}
+
+// == STOP ==
+ else if (drive == "S"){
+ smarti.setLEDs([0,50,0],[0,50,0]);
+ smarti.setMotor(1,0);
+ smarti.setMotor(2,0);
+ smarti.setMotor(3,0);
+ smarti.setMotor(4,0);
+ smarti.setServo(1,65);
+ smarti.setServo(2,35);
+ } 
+}
+
+setInterval(function() {
+ if (counting == true){
+ count = count + 1;
+ }
+ if (count > step){
+ counting = false;
+ count = 0;
+ smarti.setMotor(1,0);
+ smarti.setMotor(2,0);
+ }
+}, 50);
+
+
